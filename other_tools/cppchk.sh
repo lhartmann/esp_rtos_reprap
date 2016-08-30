77f971e372311a57ae541abd2c7cbdc443cc129d
Lucas Hartmann <lucas.hartmann@gmail.com>
2016-08-30 09:51:14 -0300

    Make cppchk.sh CHECK not fix. Requires --fix to actually fix.

--- other_tools/cppchk.sh
+++ other_tools/cppchk.sh
@@ -1,21 +1,31 @@
 #! /bin/bash
-find . -iname "*.h" | while read F; do
-	grep -q "extern \"C\"" "$F" || {
-		echo "$F is missing C++ protection, adding..."
-		
-		DATA="$(
-			echo "#ifdef __cplusplus"
-			echo "extern \"C\" {"
-			echo "#endif"
-			
-			cat "$F"
+fix="false"
+[ "$1" == "--fix" ] && fix="true"
 
-			echo ""
-			echo "#ifdef __cplusplus"
-			echo "}"
-			echo "#endif"
-		)"
-		
-		echo "$DATA" > "$F"
+find . -iname "*.h" | while read F; do
+	grep -q "extern \"C\"" "$F" \
+	&& echo "$F is ok." \
+	|| {
+		echo -n "$F is missing C++ protection"
+		if $fix; then 
+			echo ", adding..."
+			
+			DATA="$(
+				echo "#ifdef __cplusplus"
+				echo "extern \"C\" {"
+				echo "#endif"
+				
+				cat "$F"
+
+				echo ""
+				echo "#ifdef __cplusplus"
+				echo "}"
+				echo "#endif"
+			)"
+			
+			echo "$DATA" > "$F"
+		else
+			echo "."
+		fi
 	}
 done
